ea75a8ce853c2971849e15a26f7f4b366d209195
vitabulva <58066945+vitabulva@users.noreply.github.com>
2024-03-03 22:53:48 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-#This is script for installation and remove over packages
+#This is script for installation and remove every packages
 
 #installation DOOM
+apt-get install xinit xterm         #otázka nestačil by jenom core? ten je menší 
+apt-get install --no-install-recommends xserver-xephyr 
+....
 
-#remove 
 
- #testing DOOM
+
+#remove - lze se inspirovat bakalařkou
+
+
+#testing DOOM
+startx
+...
