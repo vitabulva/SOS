b5863700be683958d715fc8f96a4dc3bff601dfd
vitabulva <58066945+vitabulva@users.noreply.github.com>
2024-03-04 23:15:18 +0100

    DOOM starter
    Panové, málem jsem umřeli, ale máme to... Tímto skriptem spustíte doom jediné co je třeba: mít ve sdílenou složku ve virtualce ze které si muže vzít soubory DOOM a pak stačí jen spustit tento skritpt .. taky nejspíše nakopírovaní ve sdílené složce a je to... ale kvůli přenositelnosti se to spouští jako
    "./doom.sh <váš věc nejspíš vboxfs> <název sdílené složky>"

---
+++ doom.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#change these variables to own
+
+DOOMPATH=/root/DOOM/  		#cesta k doom, kde bude uložen
+DISK=$1				#druh pripojeni virtdisku vbox=> "vboxsf" qemu "virtiofs"
+MOUNTDISK=$2			#název sharedfolder moje je "mount_disk"
+
+apt-get update
+apt-get install -y xinit xterm
+apt-get install --no-install-recommends -y xserver-xephyr
+
+mkdir -p $DOOMPATH
+mount -t $DISK $MOUNTDISK $DOOMPATH
+
+#vytvoreni scriptu ktery se spusti se startx
+cat <<< '				
+Xephyr :1 -ac -screen 640x480x8 &
+sleep 2
+export DOOMWADDIR=/mnt/DOOM/DOOM/linuxdoom-1.10/linux
+export DISPLAY=:1
+cd $DOOMWADDIR
+./linuxxdoom
+' > /root/.xinitrc
+
+startx
